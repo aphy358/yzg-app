ca5a2db4f82f1fba7b9a46c481751d7dac518c3f
Joker <chenhua385148231@126.com>
2018-05-07 17:52:06 +0800

    11

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker';
 import App from './pages/first'
-import rootReducer from './reducers'
-
-const store = createStore(rootReducer)
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
