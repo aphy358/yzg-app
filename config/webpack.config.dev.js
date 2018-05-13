aea4648eb6cf0fddaede6b51b853e5731d46b756
aphy358 <358937572@qq.com>
2018-05-13 22:41:47 +0800

    整理路由、layout

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -158,21 +158,21 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.(css|less|scss)$/,
+            test: /\.css$/,
             use: [
+              // "style" loader turns CSS into JS modules that inject <style> tags.
               require.resolve('style-loader'),
-              require.resolve('css-loader'),
               {
+                // "css" loader resolves paths in CSS and adds assets as dependencies.
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
                 },
               },
               {
+                // "postcss" loader applies autoprefixer to our CSS.
                 loader: require.resolve('postcss-loader'),
                 options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
@@ -188,11 +188,36 @@ module.exports = {
                   ],
                 },
               },
+            ],
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
               {
-                loader: require.resolve('less-loader'),
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
               },
               {
                 loader: require.resolve('sass-loader'),
+                options: {
+                  modifyVars: { "@primary-color": "#1890ff" },
+                },
               },
             ],
           },
@@ -206,7 +231,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.less$/, /\.scss$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,39 +1,79 @@
 
+import 'antd/dist/antd.css'; 
+import './layout.scss'
+
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { Layout, Menu, Icon } from 'antd';
+import hashHistory from '../history'
 
-import LeftNav from './leftNav'
-
-
-@connect(
-    (state, props) => ({})
-)
-export default class Layout extends Component{
-
-    constructor(props, context){
-        super(props)
-    }
-
-    componentDidMount(){
-
-    }
+const { Header, Sider, Content, Footer } = Layout;
 
 
-    componentWillUnmount(){
+// 注意，这里的类名 layout 不能是大写，不然和上面定义的 Layout 变量冲突了，会报错
+export default class layout extends Component {
+	constructor(props, context){
+		super(props)
+		this._handleClick = this._handleClick.bind(this)
+	}
 
-    }
+	_handleClick = (path) => {
+		hashHistory.push(path)
+	}
 
+	state = {
+		collapsed: false,
+	};
 
-    render(){
-        const { location, children } = this.props
-        console.log('layout')
-        return (
-            <div>
-                <LeftNav location={location} />
-                <div>
-                    {children}
-                </div>
-            </div>
-        )
-    }
+	toggle = () => {
+		this.setState({
+			collapsed: !this.state.collapsed,
+		});
+	}
+
+	render() {
+		const { location, children } = this.props
+		
+		return (
+			<Layout>
+				<Sider
+					trigger={null}
+					collapsible
+					collapsed={this.state.collapsed}
+				>
+					<div className="logo" />
+					<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+						<Menu.Item key="1" onClick={() => this._handleClick('/first')}>
+							<Icon type="user" />
+							<span>首页</span>
+						</Menu.Item>
+
+						<Menu.Item key="2" onClick={() => this._handleClick('/second')}>
+							<Icon type="video-camera" />
+							<span>第二页</span>
+						</Menu.Item>
+
+						<Menu.Item key="3" onClick={() => this._handleClick('/third')}>
+							<Icon type="upload" />
+							<span>最后一页</span>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout>
+					<Header style={{ background: '#fff', padding: 0 }}>
+						<Icon
+							className="trigger"
+							type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+							onClick={this.toggle}
+						/>
+					</Header>
+					<Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+						{children}
+					</Content>
+					<Footer style={{ textAlign: 'center' }}>
+						Ant Design ©2018 Created by JL-Tour front-end team
+					</Footer>
+				</Layout>
+			</Layout>
+		);
+	}
 }
